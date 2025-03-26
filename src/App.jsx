121b618822b6fbf0f25f782a9101e0d47c508fde
ShivamIsHere <singhkumarshivam15@gmail.com>
2025-03-26 06:33:40 +0530

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Body from "./components/Body"
 import Login from "./components/Login"
-import Signup from "./components/Signup"
 import Profile from "./components/Profile"
 import { Provider } from "react-redux"
 import appStore from "./utils/appStore"
@@ -19,13 +18,10 @@ function App() {
     <Provider store={appStore}>
       <BrowserRouter basename="/">
         <Routes>
-        <Route path="/login" element={<Login />} />   
-        <Route path="/signup" element={<Signup />} />
           <Route path="/" element={<Body/>} >
             <Route path="/" element={<Home/>} />
             <Route path="/feed" element={<Feed/>} />
             <Route path="/login" element={<Login/>} />
-            <Route path="/signup" element={<Signup/>} />
             <Route path="/profile" element={<Profile/>} />
             <Route path="/connections" element={<Connections/>} />
             <Route path="/requests" element={<Requests/>} />
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,21 +5,47 @@ import { addUser } from "../utils/userSlice";
 import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../utils/constants";
 
-const Login = ({ toggleForm }) => {
+const Login = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
   const [emailId, setEmailId] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [isLoginForm, setIsLoginForm] = useState(true);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  // Handle login form submission
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(`${BASE_URL}/login`, { emailId, password }, { withCredentials: true });
+      const res = await axios.post(
+        `${BASE_URL}/login`, 
+        { emailId, password },
+        { withCredentials: true }
+      );
       dispatch(addUser(res.data)); 
-      navigate("/feed"); 
+      navigate("/feed");
+    } catch (err) {
+      setErrorMessage(err?.response?.data?.message || "Something went wrong!");
+      console.error(err);
+    }
+  };
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        `${BASE_URL}/signup`, 
+        { emailId, password, firstName, lastName, age, gender },
+        { withCredentials: true }
+      );
+      dispatch(addUser(res.data));
+      navigate("/profile"); 
     } catch (err) {
       setErrorMessage(err?.response?.data?.message || "Something went wrong!");
       console.error(err);
@@ -30,42 +56,148 @@ const Login = ({ toggleForm }) => {
     <div className="flex flex-wrap">
       <div className="flex w-full flex-col md:w-1/2 bg-base-200">
         <div className="lg:w-[28rem] mx-auto my-auto flex flex-col justify-center pt-8 md:justify-start md:px-6 md:pt-16 mt-6">
-          <p className="text-left text-3xl font-bold">Welcome 🙏, DevTinder</p>
-          <p className="mt-2 text-left text-gray-500">Welcome back, please enter your details.</p>
+          <div>
+            {isLoginForm ? (
+              <p className="text-left text-3xl font-bold">
+                Welcome 🙏, DevBharat
+              </p>
+            ) : (
+              <p className="text-left text-3xl font-bold">Register</p>
+            )}
+          </div>
 
-          <form className="flex flex-col pt-4 space-y-4" onSubmit={handleLogin}>
-            <label>Email</label>
-            <input
-              type="email"
-              value={emailId}
-              onChange={(e) => setEmailId(e.target.value)}
-              className="input input-bordered w-full max-w-xs"
-              placeholder="Email"
-            />
+          <p className="mt-2 text-left text-gray-500">
+            Welcome back, please enter your details.
+          </p>
 
-            <label>Password</label>
-            <div className="relative">
-              <input
-                type={showPassword ? "text" : "password"}
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="input input-bordered w-full max-w-xs"
-                placeholder="Password"
-              />
-              <button type="button" onClick={() => setShowPassword(!showPassword)} className="absolute right-4 text-gray-500">
-                {showPassword ? "Hide" : "Show"}
-              </button>
+          <div className="flex flex-col pt-3 md:pt-8">
+            {/* SignUp Form */}
+            {!isLoginForm && (
+              <>
+                <div className="grid gap-3 md:grid-cols-2">
+                  {/* First Name */}
+                  <div>
+                    <label>First Name</label>
+                    <input
+                      type="text"
+                      value={firstName}
+                      onChange={(e) => setFirstName(e.target.value)}
+                      placeholder="Your Name"
+                      className="mt-2 h-12 w-full rounded-md bg-gray-100 px-3"
+                    />
+                  </div>
+                  {/* Last Name */}
+                  <div>
+                    <label>Last Name</label>
+                    <input
+                      type="text"
+                      value={lastName}
+                      onChange={(e) => setLastName(e.target.value)}
+                      placeholder="Last Name"
+                      className="mt-2 h-12 w-full rounded-md bg-gray-100 px-3"
+                    />
+                  </div>
+
+                  <div>
+                    <label>Gender</label>
+                    <select
+                      value={gender}
+                      onChange={(e) => setGender(e.target.value)}
+                      className="mt-2 h-12 w-full rounded-md bg-gray-100 px-3"
+                    >
+                      <option value="">Select Gender</option>
+                      <option value="male">Male</option>
+                      <option value="female">Female</option>
+                      <option value="other">Others</option>
+                    </select>
+                  </div>
+
+                  <div>
+                    <label>Age</label>
+                    <input
+                      type="text"
+                      value={age}
+                      onChange={(e) => setAge(e.target.value)}
+                      placeholder="Enter your age"
+                      className="mt-2 h-12 w-full rounded-md bg-gray-100 px-3"
+                    />
+                  </div>
+                </div>
+              </>
+            )}
+
+            <div>
+              <div className="flex flex-col pt-4">
+                <label>Email</label>
+                <div className="focus-within:border-b-gray-500 relative flex overflow-hidden border-b-2 transition">
+                  <input
+                    type="email"
+                    value={emailId}
+                    onChange={(e) => setEmailId(e.target.value)}
+                    id="login-email"
+                    className="w-full flex-1 appearance-none border-gray-300 bg-base-100 px-4 py-2 text-base text-white placeholder-gray-400 focus:outline-none"
+                    placeholder="Email"
+                  />
+                </div>
+              </div>
+              <div className="mb-12 flex flex-col pt-4">
+                <label>Password</label>
+                <div className="focus-within:border-b-gray-500 relative flex overflow-hidden border-b-2 transition">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    id="login-password"
+                    className="w-full flex-1 appearance-none border-gray-300 bg-base-100 px-4 py-2 text-base text-white placeholder-gray-400 focus:outline-none"
+                    placeholder="Password"
+                  />
+                  <button
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="absolute right-4 text-gray-500"
+                  >
+                    {showPassword ? "Hide" : "Show"}
+                  </button>
+                </div>
+              </div>
+              {errorMessage && (
+                <div className="text-red-500 text-sm mb-4">{errorMessage}</div>
+              )}
             </div>
 
-            {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+            <button
+              onClick={isLoginForm ? handleLogin : handleSignUp}
+              className="w-full rounded-lg bg-blue-700 px-4 py-2 text-center text-base font-semibold text-white shadow-md ring-gray-500 ring-offset-2 transition focus:ring-2"
+            >
+              {isLoginForm ? "Login" : "Sign Up"}
+            </button>
+          </div>
 
-            <button className="btn btn-primary w-full" type="submit">Login</button>
-          </form>
-
-          <p className="m-auto cursor-pointer py-2 text-gray-500">
-            Don't have an account? <span className="text-blue-500 underline cursor-pointer" onClick={toggleForm}>Sign up for free.</span>
-          </p>
+          <div className="py-12 text-center">
+            <p className="whitespace-nowrap text-gray-500">
+              {isLoginForm ? "Don't have an account? " : "Existing User? "}
+              <span
+                className="underline-offset-4 md:mx-2 font-semibold text-gray-200 underline cursor-pointer"
+                onClick={() => setIsLoginForm(!isLoginForm)}
+              >
+                {isLoginForm ? "Sign up for free." : "Login Here"}
+              </span>
+            </p>
+          </div>
         </div>
+      </div>
+
+      {/* Right Side image */}
+      <div className="pointer-events-none relative hidden h-screen select-none md:block md:w-1/2">
+        <img
+          className="-z-1 absolute h-full w-full object-cover object-center opacity-90"
+          src={
+            isLoginForm
+              ? "https://i.pinimg.com/originals/06/aa/40/06aa408f09f394c3b46d6cbe1efad944.gif"
+              : "https://i.pinimg.com/originals/a4/07/22/a4072206392b57e4b3ede6588e81d7f3.gif"
+          }
+          alt="Background"
+        />
       </div>
     </div>
   );
